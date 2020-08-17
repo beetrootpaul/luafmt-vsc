09c2f0f2cff85050cb1d2753f0cfb249d1061872
Lars Mueller <appgurulars@gmx.de>
2020-08-17 22:35:23 +0200

    Initial release

---
+++ Readme.md
@@ -0,0 +1,7 @@
+# Luafmt for Visual Studio Code
+
+A Lua formatter / pretty printer for Visual Studio Code.
+
+## About
+
+Allows using the [`luafmt`](https://github.com/appgurueu/luafmt) formatter, written by appgurueu and licensed under the MIT license.
---
+++ extension.js
@@ -0,0 +1,15 @@
+const {
+	languages,
+	Range,
+	TextEdit
+} = require("vscode");
+const {
+	formatChunk
+} = require("@appguru/luafmt");
+
+module.exports = {
+	activate: context => context.subscriptions.push(languages.registerDocumentFormattingEditProvider("lua", {
+			provideDocumentFormattingEdits: document => [TextEdit.replace(new Range(document.lineAt(0).range.start, document.lineAt(document.lineCount - 1).range.end), formatChunk(document.getText()))]
+	})),
+	deactivate: () => {}
+};
