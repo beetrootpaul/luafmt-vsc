0db528d1b951cd357de20be644841d1e2210c3ae
Beetroot Paul <paul@beetrootpaul.com>
2022-10-14 00:53:01 +0200

    wip

--- extension.js
+++ extension.js
@@ -4,13 +4,24 @@ const {
 	Range,
 	TextEdit
 } = require("vscode");
-const {
-	formatChunk
-} = require("@appguru/luafmt");
+
+// const {
+// 	formatChunk
+// } = require("luafmt-pico8");
 
 module.exports = {
-	activate: context => context.subscriptions.push(languages.registerDocumentFormattingEditProvider("lua", {
-		provideDocumentFormattingEdits: document => [TextEdit.replace(new Range(document.lineAt(0).range.start, document.lineAt(document.lineCount - 1).range.end), formatChunk(document.getText()))]
+	activate: context => context.subscriptions.push(
+		languages.registerDocumentFormattingEditProvider("lua", {
+			provideDocumentFormattingEdits: document => [
+				TextEdit.replace(
+					new Range(
+						document.lineAt(0).range.start,
+						document.lineAt(document.lineCount - 1).range.end
+					),
+					"abc\ndef\n\tghi"
+					// formatChunk(document.getText())
+				)
+			]
 	})),
 	deactivate: () => {}
 };
